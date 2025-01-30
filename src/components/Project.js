c98268abcacf94bf65f339c1cec664d49842c529
Joel David <106955759+Daveside9@users.noreply.github.com>
2025-01-30 23:00:04 +0100

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -82,7 +82,7 @@ const Project = () => {
             This web application tracks grain prices (such as Rice, Ginger, Maize, and Cocoa) in Nigeria. Users can monitor and analyze
             prices dynamically.
           </p>
-          <a href="http://127.0.0.1:5000/grains" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/Daveside9/grain-data-price">
             Visit Grain Price Tracker
           </a>
         </div>
